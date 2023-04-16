6d4a52c64db4ac77a8a11f472624106787fbdb4e
Divante Parness <divante111@gmail.com>
2023-04-16 00:56:54 -0500

    did some extra work

--- prepare.py
+++ prepare.py
@@ -28,3 +28,31 @@ def split_data(df, target):
                                        )
     
     return train, validate, test
+
+
+
+def prep_telco(data):
+    data = data.drop(columns = ['payment_type_id', 'internet_service_type_id',
+                     'contract_type_id','Unnamed: 0'])
+    the_columns = data.select_dtypes('object').columns
+
+    the_columns = the_columns.drop(['customer_id', 'total_charges'])
+    dummy = pd.get_dummies(data, columns = the_columns, drop_first=True)
+    return dummy 
+
+
+
+def prep_titanic(raw_titanic_df):
+    titanic = acquire.get_titanic_data() 
+    titanic = titanic.drop(columns=['passenger_id','class','embarked','deck'])
+    dummy = pd.get_dummies(titanic[['sex', 'embark_town']], drop_first=True)
+    titanic = pd.concat([titanic, dummy], axis=1)
+    titanic = titanic.drop(columns=['sex', 'embark_town'])
+    return titanic
+
+def prep_iris(iris_df):
+    iris_df = iris_df.rename(columns={"species_name": "species"})
+    iris_df = iris_df.drop(columns=['Unnamed: 0','species_id','measurement_id'])
+    dummy_df = pd.get_dummies(iris_df[['species']])
+    iris_df = pd.concat([iris_df,dummy_df], axis=1)
+    return iris_df
